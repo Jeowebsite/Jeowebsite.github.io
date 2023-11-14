c907c174321aa64a9adedbfdebf1d79ef49dbf9f
Jeowebsite <131482745+Jeowebsite@users.noreply.github.com>
2023-11-14 03:39:57 +0000

    neq

--- retro.js
+++ retro.js
@@ -1,2 +1,2 @@
 function redirectToPage() {
-    window.location.href = "Retro-Games.html"; // Replace "anotherPage.html" with the URL of the page you want to redirect to
+    window.location.href = "https://jeowebsite.github.io/"; // Replace "anotherPage.html" with the URL of the page you want to redirect to
