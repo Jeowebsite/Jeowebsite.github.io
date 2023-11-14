97adc1fc0e4399b92ecdbf538e4dd65b9963d1b5
Jeowebsite <131482745+Jeowebsite@users.noreply.github.com>
2023-11-14 03:42:06 +0000

    new

--- retro.js
+++ retro.js
@@ -1,2 +1,8 @@
-function redirectToPage() {
-    window.location.href = "https://jeowebsite.github.io/"; // Replace "anotherPage.html" with the URL of the page you want to redirect to
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('redirectButton');
+    button.addEventListener('click', redirectToPage);
+  
+    function redirectToPage() {
+      window.location.href = "https://jeowebsite.github.io/Retro-Games.html"; // Replace "anotherPage.html" with the URL of the page you want to redirect to
+    }
+  });
