96a96f10bf8c0dbe3cbb6601455c361cb8d315ce
Jeo <131482745+Jeowebsite@users.noreply.github.com>
2023-12-02 00:30:25 +0000

    nw s

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-// Dark Mode Switch JavaScript
-
-// Function to set the theme
+// Function to set the theme and update game card background color
 function setTheme(theme) {
-    document.body.className = theme;
-    localStorage.setItem("theme", theme);
+  document.body.className = theme;
+  localStorage.setItem("theme", theme);
+
+  // Update game card background color based on the theme
+  const gameCard = document.querySelector('.game-card');
+  if (theme === 'dark-mode') {
+    gameCard.style.backgroundColor = '#222'; // Dark mode background color
+  } else {
+    gameCard.style.backgroundColor = 'rgb(255, 255, 255)'; // Light mode background color
   }
-  
-  // Check for the saved dark mode preference
-  const currentTheme = localStorage.getItem("theme");
-  const darkModeToggle = document.getElementById("dark-mode-toggle");
-  
-  // Apply the user's preferred theme
-  if (currentTheme === "dark") {
+}
+
+// Check for the saved dark mode preference
+const currentTheme = localStorage.getItem("theme");
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+// Apply the user's preferred theme and update game card background color
+if (currentTheme === "dark-mode") {
+  setTheme("dark-mode");
+  darkModeToggle.checked = true;
+} else {
+  setTheme("light-mode");
+  darkModeToggle.checked = false;
+}
+
+// Toggle dark mode on checkbox change
+darkModeToggle.addEventListener("change", () => {
+  if (darkModeToggle.checked) {
     setTheme("dark-mode");
-    darkModeToggle.checked = true;
   } else {
     setTheme("light-mode");
-    darkModeToggle.checked = false;
   }
-  
-  // Toggle dark mode on checkbox change
-  darkModeToggle.addEventListener("change", () => {
-    if (darkModeToggle.checked) {
-      setTheme("dark-mode");
-    } else {
-      setTheme("light-mode");
-    }
-  });
-  
+});
+      
