43fd0de0c629b854ccd34b9c86665fefae66a075
Jeo <131482745+Jeowebsite@users.noreply.github.com>
2023-12-12 20:32:51 -0600

    new

--- openURLInWindow.js
+++ openURLInWindow.js
@@ -3,7 +3,7 @@ function openMyWindow(url) {
     myWindow.document.write('<!DOCTYPE html>\n\
     <html>\n\
     <head>\n\
-    <title>Dynamic Iframe</title>\n\
+    <title>Jeo Website Games</title>\n\
     </head>\n\
     <body style="margin: 0;">\n\
     <iframe src="' + url + '" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0;left:0;right:0;bottom:0;" height="100%" width="100%">\n\
