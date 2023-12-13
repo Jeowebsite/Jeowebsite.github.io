9a52a42a0ddfc3d37247501cbc0f242d69ca50a8
Jeo <131482745+Jeowebsite@users.noreply.github.com>
2023-12-12 19:56:14 -0600

    new

--- openURLInWindow.js
+++ openURLInWindow.js
@@ -1,4 +1,4 @@
-function url(url) {
+function openURLInWindow(targetURL) {
     var myWindow = window.open('', 'myWindow', 'scrollbars=1,height=' + screen.availHeight + ',width=' + screen.availWidth);
     myWindow.document.write('<!DOCTYPE html>\n\
     <html>\n\
@@ -6,7 +6,7 @@ function url(url) {
     <title>Dynamic Iframe</title>\n\
     </head>\n\
     <body style="margin: 0;">\n\
-    <iframe src="' + url + '" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0;left:0;right:0;bottom:0;" height="100%" width="100%">\n\
+    <iframe src="' + targetURL + '" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0;left:0;right:0;bottom:0;" height="100%" width="100%">\n\
     </iframe>\n\
     </body>\n\
     </html>');
