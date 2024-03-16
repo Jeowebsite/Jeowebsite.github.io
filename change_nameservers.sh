fff5198eeac96e70417329e49c9324661b699200
Jeo <131482745+Jeowebsite@users.noreply.github.com>
2024-03-15 19:45:54 -0500

    pls man

--- change_nameservers.sh
+++ change_nameservers.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
+name: Change Name Servers to Ezoic
 
-# Set executable permissions for the script
-chmod +x change_nameservers.sh
+on:
+  push:
+    branches:
+      - main  # Change this to your default branch if different
 
-# Domain name
-DOMAIN="jeowebsite.github.io"
+jobs:
+  change_name_servers:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Repository
+        uses: actions/checkout@v2
 
-# Ezoic name servers
-EZOID_NAMESERVER1="falcon.ezoicns.com"
-EZOID_NAMESERVER2="rabbit.ezoicns.com"
-EZOID_NAMESERVER3="somali.ezoicns.com"
-EZOID_NAMESERVER4="turkey.ezoicns.com"
+      - name: Set executable permissions for script
+        run: chmod +x change_nameservers.sh
 
-# Registrar API Key
-REGISTRAR_API_KEY="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"
-
-# Command to change name servers using registrar's API
-# This is just a placeholder example, replace it with the actual command or API call to your domain registrar
-echo "Changing name servers for domain $DOMAIN to $EZOID_NAMESERVER1, $EZOID_NAMESERVER2, $EZOID_NAMESERVER3, and $EZOID_NAMESERVER4"
-# Example command:
-# registrar_api_command --domain "$DOMAIN" --nameservers "$EZOID_NAMESERVER1,$EZOID_NAMESERVER2,$EZOID_NAMESERVER3,$EZOID_NAMESERVER4" --apikey "$REGISTRAR_API_KEY"
+      - name: Run Script to Change Name Servers
+        run: ./change_nameservers.sh $DOMAIN $EZOID_NAMESERVER1 $EZOID_NAMESERVER2 $EZOID_NAMESERVER3 $EZOID_NAMESERVER4
+        env:
+          DOMAIN: "jeowebsite.github.io"
+          EZOID_NAMESERVER1: "falcon.ezoicns.com"
+          EZOID_NAMESERVER2: "rabbit.ezoicns.com"
+          EZOID_NAMESERVER3: "somali.ezoicns.com"
+          EZOID_NAMESERVER4: "turkey.ezoicns.com"
+          REGISTRAR_API_KEY: "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"
