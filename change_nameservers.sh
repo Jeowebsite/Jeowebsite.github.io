efe257cd13831d31f9decd7d7a58abc6eebdc173
Jeo <131482745+Jeowebsite@users.noreply.github.com>
2024-03-15 19:39:45 -0500

    PLEASE WORK I NEED MONEY

--- change_nameservers.sh
+++ change_nameservers.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set executable permissions for the script
+chmod +x change_nameservers.sh
+
 # Domain name
 DOMAIN="jeowebsite.github.io"
 
@@ -13,7 +16,7 @@ EZOID_NAMESERVER4="turkey.ezoicns.com"
 REGISTRAR_API_KEY="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"
 
 # Command to change name servers using registrar's API
-# This is a placeholder example; replace it with the actual command or API call to your domain registrar
+# This is just a placeholder example, replace it with the actual command or API call to your domain registrar
 echo "Changing name servers for domain $DOMAIN to $EZOID_NAMESERVER1, $EZOID_NAMESERVER2, $EZOID_NAMESERVER3, and $EZOID_NAMESERVER4"
 # Example command:
 # registrar_api_command --domain "$DOMAIN" --nameservers "$EZOID_NAMESERVER1,$EZOID_NAMESERVER2,$EZOID_NAMESERVER3,$EZOID_NAMESERVER4" --apikey "$REGISTRAR_API_KEY"
